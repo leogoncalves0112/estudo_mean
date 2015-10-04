75b6cac3d1c1dbddffb0c4a86197a269e1256513
Leonardo Goncalves <leo.goncalves0112@gmail.com>
2015-10-04 13:59:05 -0300

    Comit inical do projedo com MEAN

---
+++ public/js/controllers/ContatosController.js
@@ -0,0 +1,54 @@
+angular.module('contatooh').controller('ContatosController',
+	function($scope, Contato) {
+		$scope.contatos = [];
+		
+		$scope.filtro = '';
+		
+		$scope.mensagem = {texto: ''};
+		
+		//var Contato = $resource('/contatos/:id');
+		
+		function buscaContatos() {
+			Contato.query(
+				function(contatos) {
+					$scope.contatos = contatos;
+					$scope.mensagem = {};
+				},
+				function(erro) {
+					console.log(erro);
+					$scope.mensagem = {texto: 'Não foi possível obter a lista'};
+				}
+			);
+		}
+		buscaContatos();
+		
+		$scope.remove = function(contato) {
+			Contato.delete({id: contato._id},
+				buscaContatos,
+				function(erro) {
+					console.log(erro);
+					$scope.mensagem = {texto: 'Não foi possível remover o contato'};
+				}
+			);
+		};
+		
+		/*var promise = Contato.query().$promise;
+		promise
+			.then(function(contatos) {
+				$scope.contatos = contatos;
+			})
+			.catch(function(erro) {
+				console.log("Não foi possível obter a lista de contatos");
+				console.log(erro);
+			});*/
+		
+		/*$http.get('/contatos')
+			.success(function(data) {
+				$scope.contatos = data;
+			})
+			.error(function(statusText) {
+				console.log("Não foi possível obter a lista de contatos");
+				console.log(statusText);
+			});*/
+
+});
